ff6b9bac6132056b754289b7eaf2b9f5435b1f92
Sakthivel R <43096472+sakthiveltsmr@users.noreply.github.com>
2022-03-27 21:14:11 +0530

    Task

---
+++ basics_js/create_varibale.js
@@ -0,0 +1,155 @@
+//comments inline , multi lines
+
+// console.log("Hello World!") // in-line comment
+
+/*  console.log("Hy");
+    console.log("Hy1");
+    console.log("!hey2") */; // multi-line comment
+
+// -----------------------------------------
+
+//2.create variable myName with var keyword;
+var myName;
+
+// -------------------------------------------------------
+
+//3. assign value 7 to variable a;
+var a;
+a = 7; // now a contains 7 value;
+
+
+// --------------------------------------------------
+
+// 4.Assigning the Value of One Variable to Another
+// Setup
+
+var a;
+a = 7;
+var b;
+// --------------------------------------------------
+
+//5. Only change code below this line
+
+b = a; // now b contains a's variables value; 7;
+
+// ------------------------------------------------------
+
+//6. Define a variable a with var and initialize it to a value of 9.
+
+var a = 9;
+
+// ------------------------------------------------------------
+
+// 7.Create two new string variables: myFirstName and myLastName and assign them the values of your first and last name, respectively.
+
+var myFirstName = "Muthu";
+var myLastName = "Manikandan";
+
+// ----------------------------------------------------------
+//8. Initialize the three variables a, b, and c with 5, 10, and "I am a" respectively so that they will not be undefined.
+
+
+// Only change code below this line
+
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+
+// --------------------------------------------------------
+
+//9. Modify the existing declarations and assignments so their names use camelCase.
+
+// Do not create any new variables.
+
+
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+// --------------------------------------------------------
+
+// 10. Update the code so it only uses the let keyword.
+let catName = "Oliver";
+let catSound = "Meow!";
+// --------------------------------------------------------
+
+// 11.var should not exist in your code.
+
+// You should change fCC to all uppercase.
+
+// FCC should be a constant variable declared with const.
+
+// fact should be declared with let.
+
+// console.log should be changed to print the FCC and fact variables.
+
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+
+// --------------------------------------------------------
+//12. Change the 0 so that sum will equal 20.
+const sum = 10 + 10;
+
+// --------------------------------------------------------
+// 13..Change the 0 so the difference is 12.
+const difference = 45 - 33;
+
+
+// --------------------------------------------------------
+// 14.Change the 0 so that product will equal 80.
+const product = 8 * 10;
+
+// --------------------------------------------------------
+// 15.Change the 0 so that the quotient is equal to 2.
+const quotient = 66 / 33;
+
+// --------------------------------------------------------
+// 16.Change the code to use the ++ operator on myVar.
+let myVar = 87;
+
+// Only change code below this line
+myVar++;
+
+// --------------------------------------------------------
+
+
+// 17.Change the code to use the -- operator on myVar.
+let myVar = 11;
+
+// Only change code below this line
+myVar--;
+// --------------------------------------------------------
+// 18.Create a variable myDecimal and give it a decimal value with a fractional part (e.g. 5.7).
+
+const ourDecimal = 5.7;
+
+// Only change code below this line
+
+const myDecimal = 5.7;
+
+
+// --------------------------------------------------------
+// 19.Change the 0.0 so that product will equal 5.0.
+const product = 2.0 * 2.5;
+
+// --------------------------------------------------------
+// 20.Change the 0.0 so that quotient will equal to 2.2.
+const quotient = 4.4 / 2.0; // Change this line
+
+// --------------------------------------------------------
+// 21.Set remainder equal to the remainder of 11 divided by 3 using the remainder (%) operator.
+const remainder = 11 % 3;
